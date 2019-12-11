f530c327468ee6d0af8830b86fd8aa24184b7cef
Jacob Sommer <jacob.sommer@comcast.net>
2019-12-10 23:31:40 -0800

    Remove unnecessary stuff

--- car.py
+++ car.py
@@ -1,23 +1,19 @@
 '''
-Program to run DIY star tracker
-Created by Jacob Sommer 2019-10-28
+RC Car program for RC League
+Created by Jacob Sommer 2019-12-10
 '''
 import math
 from time import sleep
-import asyncio
 import RPi.GPIO as GPIO
-from multiprocessing import Process
 from flask import Flask
 from flask import request
 
 # define the ports the IN1-4 for the motor board are connected to
-# based on the number coming after GPIO ex: IN1 is connected to port 11/GPIO17 which is 17
+# based on the number coming after GPIO (BCM numbering mode) ex: IN1 is connected to port 11/GPIO17 which is 17
 IN1 = 17
 IN2 = 27
 IN3 = 22
 IN4 = 23
-DELAY = 0.001
-procs = []
 
 # set up GPIO
 def setupGPIO():
@@ -45,7 +41,6 @@ def drive(value):
   sleep(math.fabs(value / 100.0))
   GPIO.output(IN1, False)
   GPIO.output(IN2, False)
-  return None
 
 def turn(value):
   if value > 0: # forward
@@ -57,7 +52,6 @@ def turn(value):
   sleep(math.fabs(value / 100.0))
   GPIO.output(IN3, False)
   GPIO.output(IN4, False)
-  return None
 
 @app.route('/drive', methods=['POST'])
 def route_drive():
@@ -68,42 +62,6 @@ def route_drive():
 def route_turn():
   turn(float(request.form['value']))
   return 'Turn'
-
-@app.route('/stopdrive')
-def stop_drive():
-  GPIO.output(IN1, False)
-  GPIO.output(IN2, False)
-  return "Stop drive"
-
-@app.route('/stopturn')
-def stop_turn():
-  GPIO.output(IN3, False)
-  GPIO.output(IN4, False)
-  return "Stop turn"
-
-@app.route('/w')
-def forward():
-  GPIO.output(IN1, False)
-  GPIO.output(IN2, True)
-  return "Forward"
-
-@app.route('/a')
-def left():
-  GPIO.output(IN3, False)
-  GPIO.output(IN4, True)
-  return "Left"
-
-@app.route('/s')
-def backward():
-  GPIO.output(IN1, True)
-  GPIO.output(IN2, False)
-  return "Backward"
-
-@app.route('/d')
-def right():
-  GPIO.output(IN3, True)
-  GPIO.output(IN4, False)
-  return "Right"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True, port=5000)
